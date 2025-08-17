f557cce462f19538408297d5c8fa8417ed753efa
Lee-Young-Taek <honbaem@gmail.com>
2025-08-17 21:56:40 +0900

    data_preprocessing update

--- Code/data_preprocessing.py
+++ Code/data_preprocessing.py
@@ -9,11 +9,14 @@ df_building_original = pd.read_csv("../Data/건축물대장_통합.csv")
 df_building_filter = df_building_original.loc[df_building_original["위도"].isna() & df_building_original["경도"].isna(), :]
 
 # 구조코드명별 퍼센트
-total = len(df_building_filter)
-summary = (df_building_filter['구조코드명']
-           .value_counts(dropna=False)
-           .rename_axis('구조코드명')
-           .reset_index(name='수')
-           .assign(퍼센트=lambda d: (d['수'] / total * 100).round(2))
-           .sort_values(['수','구조코드명'], ascending=[False, True], ignore_index=True))
-summary
+df_building_grouped = (df_building_filter
+                       .groupby('구조코드명', dropna=False)
+                       .size()
+                       .rename('수')
+                       .reset_index())
+
+df_building_grouped['퍼센트'] = (df_building_grouped['수'] / len(df_building_filter) * 100).round(2)
+df_building_grouped = df_building_grouped.sort_values(['수','구조코드명'], ascending=[False, True], ignore_index=True)
+
+df_building_grouped
+
